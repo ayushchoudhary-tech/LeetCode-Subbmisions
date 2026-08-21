d03454c8a46d954b5f81fcbae984af3650cb7c85
ayushchoudhary-tech <ayushchoudhary.us18@gmail.com>
2026-08-21 11:32:52 +0530

    Time: 47 ms (63.62%), Space: 29 MB (73.22%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -21,7 +21,8 @@ public:
                while(j<k && nums[j]==nums[j-1])
                {j++;
                 }
-               
+                while(j<k && nums[k]==nums[k+1]){k--;}
+                
             }
         }}
         return ans;
