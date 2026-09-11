a96223262a6f7b2a0994f180742585d54f5ef856
ayushchoudhary-tech <ayushchoudhary.us18@gmail.com>
2026-09-11 20:41:23 +0530

    Time: 0 ms (100%), Space: 10.4 MB (58.16%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -4,16 +4,19 @@ public:
         int n=s.length(); string res="";
         reverse(s.begin(),s.end());
         for(int i=0;i<n;i++)
-        {  string word="";
-            while(s[i]!=' ' &&  i<n)
-            { word+=s[i];
-                 i++;
-            } 
-            if(word.length()>0)
-            {reverse(word.begin(),word.end());
-                res+=" "+word;
-            }
-        }
+      { string word="";
+       while(s[i]!=' ' && i<n)
+       {
+         word+=s[i];
+         i++;
+       }
+       reverse(word.begin(),word.end());
+       if(word.length()>0)
+       { res+=" "+word;
+
+       }
+
+      }
         return res.substr(1);
     }
 };
