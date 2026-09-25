22b02caba16ecaf281e61cfacc2a8f33b31e85b5
ayushchoudhary-tech <ayushchoudhary.us18@gmail.com>
2026-09-25 21:18:43 +0530

    Time: 22 ms (60.7%), Space: 17.4 MB (87.5%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -7,28 +7,28 @@ public:
         for(int i=0;i<n;i++)
         {  if(i>0 && nums[i]==nums[i-1]){continue;}
             for(int j=i+1;j<n;j++)
-            {  if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                int k=j+1,l=n-1;
-             while(k<l)
+            { if(j>i+1 && nums[j]==nums[j-1]){continue;}
+            int k=j+1; int l=n-1;
+            while(k<l)
+            { long long  sum=(long long)nums[i]+(long long)nums[j]+(long long)nums[k]+(long long)nums[l];
+             if(sum<target)
              {
-                long long sum=(long long)nums[i]+(long long)nums[j]+(long long)nums[k]+(long long)nums[l];
-                if(sum<target)
-                {
-                    k++;
-                }else if(sum>target)
-                { l--;
+                k++;
+             }else if(sum>target)
+             {
+                l--;
+             }
+             else{
+                ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                k++;l--;
+                while(k<l && nums[k]==nums[k-1]){k++;}
+                while(l>k &&  nums[l]==nums[l+1]){l--;}
+             }
 
-                }else {
-                    ans.push_back({nums[i],nums[j],nums[k],nums[l]});
-                    k++;l--;
-                    while(k<l && nums[k]==nums[k-1]){k++;}
-                    while(k<l && nums[l]==nums[l+1]){l--;}
-                }
-               
-             } 
-             
+
 
             }
+        }
         } 
         return ans;
     }
